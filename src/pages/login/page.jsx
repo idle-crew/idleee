34a7f94902855d30210dafc8276d770b4e9ddfe7
junghongseop <a01082372487@gmail.com>
2025-01-04 12:42:32 +0900

    feat: 로그인 페이지 개발

--- src/pages/login/page.jsx
+++ src/pages/login/page.jsx
@@ -1,5 +1,38 @@
+import styled from "styled-components";
+import IconLogin from "../../icons/IconLogin";
+import LoginButton from "../../components/login/LoginButton/LoginButton";
+
 const Login = () => {
-  return <>loin</>;
+  return (
+    <StyledLoginPage>
+      <IconWrapper>
+        <IconLogin />
+      </IconWrapper>
+      <StyledLogin>SNS로 간편 가입하기</StyledLogin>
+      <LoginButton />
+    </StyledLoginPage>
+  );
 };
 
 export default Login;
+
+const StyledLoginPage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+`;
+
+const IconWrapper = styled.div`
+  padding-bottom: 201px;
+`;
+
+const StyledLogin = styled.div`
+  font-size: 20px;
+  color: white;
+  font-weight: 500;
+  padding-bottom: 21px;
+`;
