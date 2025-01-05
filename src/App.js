c9cd90ecc366dfb799cffc583c0db1c38775e5b0
junghongseop <a01082372487@gmail.com>
2025-01-06 02:46:30 +0900

    fix: 전역 리코일세팅

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Main from './pages/main/page';
-import Login from './pages/login/page';
-import PostDetail from './pages/post/page';
-import Edit from './pages/edit/page';
-import Upload from './pages/upload/page';
-import './App.css';
+import Main from "./pages/main/page";
+import Login from "./pages/login/page";
+import PostDetail from "./pages/post/page";
+import Edit from "./pages/edit/page";
+import Upload from "./pages/upload/page";
+import "./App.css";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/post/:id" element={<PostDetail />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/upload" element={<Upload />} />
-      </Routes>
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post/:id" element={<PostDetail />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/upload" element={<Upload />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
   );
 }
 
